bc7c38c6974902946edb422c2356b5ee9cee555c
Thamsanqa Mbexe <thamsanqambexe@gmail.com>
2021-08-11 19:45:07 +0200

    converting temperature.

---
+++ level-07.js
@@ -0,0 +1,9 @@
+function convertCelsius(c) {
+    var fahrenheit = ( c * 9 / 5) + 32;
+    return fahrenheit;
+}
+
+function convertFahrenheit(f) {
+    var celsius = (f - 32) * 5 / 9;
+    return celsius;
+}
